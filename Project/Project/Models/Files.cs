6d5c0c87c132141554d58f1258f51a416f33a54f
Daniel Richards <daniel.richards-8@students.plymouth.ac.uk>
2020-02-25 15:57:47 +0000

    Added Files model class. Will now add attributes to it.

---
+++ Project/Project/Models/Files.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace Project.Models
+{
+    public class Files
+    {
+    }
+}
