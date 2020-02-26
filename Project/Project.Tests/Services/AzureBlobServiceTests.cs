12d223a2976d00485f9c6558326ab84d3987615a
Daniel Richards <daniel.richards-8@students.plymouth.ac.uk>
2020-02-26 16:07:32 +0000

    Created Working Unit Test for GetClient method for Azure Blobs.

---
+++ Project/Project.Tests/Services/AzureBlobServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage;
+using Project.Services;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Services.Tests
+{
+    [TestClass()]
+    public class AzureBlobServiceTests
+    {
+        [TestMethod()]
+        public void GetBlobContainerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetClient()
+        {
+            var connectionStringConfiguration = ConfigurationManager.ConnectionStrings["StorageClient"].ConnectionString;
+            var cloudStorageConnection = CloudStorageAccount.Parse(connectionStringConfiguration);
+            var blobClient = cloudStorageConnection.CreateCloudBlobClient();
+
+            Assert.IsNotNull(blobClient);
+        }
+
+        [TestMethod()]
+        public void UploadAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteAllAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Project/Project/Services/AzureBlobService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Project.Services
+{
+    interface IAzureBlobService
+    {
+    }
+
+    public class AzureBlobService : IAzureBlobService
+    {
+        private string storageConnectionString = ConfigurationManager.ConnectionStrings["StorageClient"].ConnectionString;
+
+        public async Task GetBlobContainer()
+        {
+
+        }
+
+        public async Task UploadAsync(IFormFileCollection files)
+        {
+
+        }
+
+        public async Task DeleteAsync(string fileUri)
+        {
+
+        }
+
+        public async Task DeleteAllAsync()
+        {
+
+        }
+    }
+}
