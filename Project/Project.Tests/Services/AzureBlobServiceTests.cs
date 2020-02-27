cceb7b02a6ae3ea7408f732ffc500f66a1532613
Daniel Richards <daniel.richards-8@students.plymouth.ac.uk>
2020-02-27 10:21:58 +0000

    Added unit test for UploadAsync method for AzureBlobsService class.

--- Project/Project.Tests/Services/AzureBlobServiceTests.cs
+++ Project/Project.Tests/Services/AzureBlobServiceTests.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using Project.Services;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -65,7 +67,30 @@ namespace Project.Services.Tests
         [TestMethod()]
         public void UploadAsyncTest()
         {
-            Assert.Fail();
+            //Test container
+            var testContainerName = "test";
+
+            //Sample blob name
+            var blob = "test-image.png";
+
+            //Path to file
+            var filePath = "C:/Users/Daniel Richards/Pictures/test-image.png";
+
+            //Create connection to client
+            var connectionStringConfiguration = ConfigurationManager.ConnectionStrings["StorageClient"].ConnectionString;
+            var cloudStorageConnection = CloudStorageAccount.Parse(connectionStringConfiguration);
+            var blobClient = cloudStorageConnection.CreateCloudBlobClient();
+
+            //Get Blob container
+            var blobContainer = blobClient.GetContainerReference(testContainerName);
+
+            //Open File and upload its data
+            FileStream uploadFileStream = File.OpenRead(filePath);
+            var blobOnContainer = blobContainer.GetBlockBlobReference(blob);
+            blobOnContainer.UploadFromFile(filePath);
+
+            //Assert blob exists on container
+            Assert.IsNotNull(blobOnContainer);
         }
 
         [TestMethod()]
