503bda19c08874a6b15763915ac4237dd6042417
Daniel Richards <daniel.richards-8@students.plymouth.ac.uk>
2020-03-02 16:16:52 +0000

    Added Files Controller and generated views.

--- Project/Project/Models/IdentityModels.cs
+++ Project/Project/Models/IdentityModels.cs
@@ -38,8 +38,8 @@ namespace Project.Models
             return new ApplicationDbContext();
         }
 
-        public System.Data.Entity.DbSet<Project.Models.Files> Files { get; set; }
+        public System.Data.Entity.DbSet<Project.Models.Files> Files { get; set; }   
 
-        
+
     }
 }
