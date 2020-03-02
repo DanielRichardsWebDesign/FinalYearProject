381954d5f5cbe0cb251258773d27eadb40b8f5a4
Daniel Richards <daniel.richards-8@students.plymouth.ac.uk>
2020-03-02 17:48:18 +0000

    Added initialisation method to Files Controller for IAzureBlobService

--- Project/Project/Controllers/FilesController.cs
+++ Project/Project/Controllers/FilesController.cs
@@ -8,19 +8,34 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Project.Models;
+using Project.Services;
+using Microsoft.AspNet.Identity;
 
 namespace Project.Controllers
 {
     public class FilesController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
+        private IAzureBlobService azureBlobService;
+
+        public FilesController() :this(new AzureBlobService())
+        {
+
+        }
+
+        public FilesController(IAzureBlobService azureBlobService)
+        {
+            this.azureBlobService = azureBlobService;
+        }
 
         // GET: Files
         public async Task<ActionResult> Index()
         {
             var files = db.Files.Include(f => f.ApplicationUser).Include(f => f.Projects);
             return View(await files.ToListAsync());
-        }        
+        }
+        
+        
 
         protected override void Dispose(bool disposing)
         {
